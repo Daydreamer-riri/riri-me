1bbcd1022d3d6cdfd0caded91d12037175efd53a
Daydreamer-riri <Daydreamerriri@outlook.com>
2023-03-14 14:02:13 +0800

    build: add rehype-autolink-headings plugin for auto-linking headings in markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import Icons from 'unplugin-icons/vite'
 import react from '@astrojs/react'
 import remarkToc from 'remark-toc'
 import remarkCollapse from 'remark-collapse'
+import rehypeAutoLindHeadings from 'rehype-autolink-headings'
 import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
@@ -12,7 +13,8 @@ export default defineConfig({
   site: 'https://riri-me.netlify.app',
   integrations: [UnoCss(), react(), sitemap()],
   markdown: {
-    remarkPlugins: [remarkToc, [remarkCollapse, { test: 'Table of content' }]],
+    remarkPlugins: [remarkToc],
+    rehypePlugins: [[rehypeAutoLindHeadings, { behavior: 'prepend' }]],
     shikiConfig: {
       wrap: true,
       theme: 'vitesse-dark',
