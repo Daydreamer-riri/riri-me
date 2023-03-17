980ea264ca1ea4c51cb8a735d2f810c509ed581d
Daydreamer-riri <Daydreamerriri@outlook.com>
2023-03-17 18:05:17 +0800

    update: projects list

--- src/content/page/_projects.tsx
+++ src/content/page/_projects.tsx
@@ -18,21 +18,21 @@ const projects: Projects = {
   title: 'Projects - Riri',
   desc: 'Riri\'s  Project List',
   projects: {
-    'CLI': [
-      {
-        name: 'ca-git',
-        desc: 'Commitizen CLI adapter',
-        iconClass: 'i-carbon-plug',
-        // icon: () => <i>1234</i>,
-        link: 'https://cz-git.qbb.sh/guide/introduction',
-      },
-      {
-        name: 'czg',
-        desc: 'Commitizen CLI alternative',
-        iconClass: 'i-carbon:terminal',
-        link: 'https://cz-git.qbb.sh/cli/',
-      },
-    ],
+    // 'CLI': [
+    //   {
+    //     name: 'ca-git',
+    //     desc: 'Commitizen CLI adapter',
+    //     iconClass: 'i-carbon-plug',
+    //     // icon: () => <i>1234</i>,
+    //     link: 'https://cz-git.qbb.sh/guide/introduction',
+    //   },
+    //   {
+    //     name: 'czg',
+    //     desc: 'Commitizen CLI alternative',
+    //     iconClass: 'i-carbon:terminal',
+    //     link: 'https://cz-git.qbb.sh/cli/',
+    //   },
+    // ],
     'VS Code Extensions:': [
       {
         name: 'Iconfont reminder',
@@ -41,14 +41,22 @@ const projects: Projects = {
         link: 'https://github.com/Daydreamer-riri/vscode-ext-iconfont-reminder',
       },
     ],
-    'Toys': [
+    'Configurations': [
       {
-        name: 'qb-coin',
-        desc: 'Manage and view BSC coins in terminal',
-        iconClass: 'i-carbon:condition-wait-point',
-        link: 'https://github.com/Zhengqbbb/qb-coin',
+        name: 'eslint-config',
+        desc: 'My ESLint config presets',
+        iconClass: 'i-simple-icons:eslint',
+        link: 'https://github.com/Daydreamer-riri/eslint-config',
       },
     ],
+    // 'Toys': [
+    //   {
+    //     name: 'qb-coin',
+    //     desc: 'Manage and view BSC coins in terminal',
+    //     iconClass: 'i-carbon:condition-wait-point',
+    //     link: 'https://github.com/Zhengqbbb/qb-coin',
+    //   },
+    // ],
   },
 }
 
